436e0321d396b835aad3b4c2201674f7346f05b0
pyguy12 <wuwarren7360@gmail.com>
2019-07-13 23:10:16 -0500

    Made public directory to house HTML files and changed index.js to make a more practical server which can handle errors and serve files as well as write meta data about the network responses. Also changed package.json's test script to a start script.

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-// Instantiating a new logger. Since Logger is a class, we must instantiate it.
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : `${req.url}`
+  );
 
-logger.on("message", args => {
-  console.log(args);
+  let extName = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (extName) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "appliaction/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content);
+    }
+  });
 });
 
-logger.log("Hello");
+const PORT = process.env.port || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
